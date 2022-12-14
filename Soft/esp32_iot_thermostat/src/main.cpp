5c7b2cfce9ec1cf861e90f3b6ee273c86df4d055
Martin Ptáček <30041036+MarPta@users.noreply.github.com>
2022-12-14 08:12:29 +0100

    Removed internet connectivity. Fixed compilation.

--- Soft/esp32_iot_thermostat/src/main.cpp
+++ Soft/esp32_iot_thermostat/src/main.cpp
@@ -1,87 +1,52 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
-#include <WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <Preferences.h>
-#include <WiFiManager.h>
 #include "Adafruit_Sensor.h"
 #include "DHT.h"
-#include <PubSubClient.h>
 
 #include <HT1621_universal.h>
-#include "Control/OnOffRegulator.h"
+#include "OnOffRegulator.h"
 #include "encoder.h"
 
 const uint8_t dhtPin = 17;
-const uint8_t displayCsPin = 14;   //Chip selection output
-const uint8_t displayWrPin = 27;   //Read clock output
-const uint8_t displayDataPin = 26; //Serial data output
+const uint8_t displayCsPin = 14;   // Chip selection output
+const uint8_t displayWrPin = 27;   // Read clock output
+const uint8_t displayDataPin = 26; // Serial data output
 const uint8_t DISPLAY_LED = 13;
-const uint16_t displayNightLightThreshold = 300; //0-4096 
-const uint16_t displayNightLightMax = 80; //0-1023
-const uint16_t displayModDelay = 3000;  //[ms]
-const uint8_t defaultLowTemp = 10;  //[°C]
-const uint8_t defaultHighTemp = 20;  //[°C]
+const uint16_t displayNightLightThreshold = 300; // 0-4096 
+const uint16_t displayNightLightMax = 80; // 0-1023
+const uint16_t displayModDelay = 3000;  // [ms]
+const uint8_t defaultLowTemp = 10;  // [°C]
+const uint8_t defaultHighTemp = 20;  // [°C]
 
-//LED PWM properties
+// LED PWM properties
 const uint16_t freq = 5000;
 const uint8_t ledChannel = 0;
 const uint8_t resolution = 10;
 
 const uint8_t photoresistorPin = 35;
 const uint8_t relayPin = 16;
-const float desiredTempMax = 30.0;   //[°C]
-const float desiredTempMin = 5.0;   //[°C]
-const float hysteresisUpTemp = 0.3;      //[°C]
-const float hysteresisDownTemp = 0.3;    //[°C]
-
-#define MQTT_SERVER "http://hassio.local:8123" //"192.168.0.6"
-#define MQTT_PORT 1883
-#define MQTT_USER "mqttUser"
-#define MQTT_PASS "mqttPass"
-#define MQTT_CLIENT_ID "CalmFire_thermostat"
-
-WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
-
-char * tempTopic = "thermostat/temperature";
-void mqttReceiving(char * topic, byte * payload, unsigned int length);
-void mqttReconnect();
-
-void mqttReconnect() {
-    while (!mqttClient.connected()) {
-        
-        Serial.print("Trying to connect to MQTT server ...");
-        if (mqttClient.connect(MQTT_CLIENT_ID))
-            Serial.println("connected");
-        else {
-            Serial.print("failed, status code =");
-            Serial.print(mqttClient.state());
-            Serial.println("trying again in 5 seconds");
-            delay(5000);
-        }
-    }
-    Serial.println("Connected to MQTT Succesfully!");
-}
+const float desiredTempMax = 30.0;   // [°C]
+const float desiredTempMin = 5.0;   // [°C]
+const float hysteresisUpTemp = 0.3;      // [°C]
+const float hysteresisDownTemp = 0.3;    // [°C]
 
 struct stateVector{
-    float actualTemp = 0.0;         //[°C]
-    float desiredTempLow = defaultLowTemp;    //[°C]
-    float desiredTempHigh = defaultHighTemp;   //[°C]
-    bool heatHigh = false;  //Choose desired temp: 0->desiredTempLow, 1->desiredTempHigh
-    float relHum = 0.0;     //[%]
+    float actualTemp = 0.0;         // [°C]
+    float desiredTempLow = defaultLowTemp;    // [°C]
+    float desiredTempHigh = defaultHighTemp;   // [°C]
+    bool heatHigh = false;  // Choose desired temp: 0->desiredTempLow, 1->desiredTempHigh
+    float relHum = 0.0;     // [%]
     bool relayOn = false;
-    int16_t illuminance = 0;   //12-bit reading
-    int16_t displayBrightness = 0;   //10-bit output
-    uint8_t displayState = 0;   //measuredTemp, desiredTemp
+    int16_t illuminance = 0;   // 12-bit reading
+    int16_t displayBrightness = 0;   // 10-bit output
+    uint8_t displayState = 0;   // 0-measuredTemp, 1-desiredTemp
 } currentStateVector;
 
 DHT dht(dhtPin, DHT22);
-WiFiManager wifiManager;
 Preferences flashPreferences;
 OnOffRegulator heatRegulator;
 HT1621_universal lcd(displayCsPin, displayWrPin, displayDataPin);
-Encoder encoder1(1);   //slot 1
+Encoder encoder1(1);
 
 template <typename T>
 T clamp(T value, T min, T max) {
@@ -92,7 +57,7 @@ T clamp(T value, T min, T max) {
 	return value;
 }
 
-void setup(){
+void setup() {
     Serial.begin(115200);
 
     lcd.init();
@@ -102,28 +67,20 @@ void setup(){
     ledcSetup(ledChannel, freq, resolution);
     ledcAttachPin(DISPLAY_LED, ledChannel);
 
-    wifiManager.autoConnect("CalmFire_thermostat", "12345678");
     dht.begin();
 
-    flashPreferences.begin("desiredTemps", false);  // RW-mode (second parameter false).
+    flashPreferences.begin("desiredTemps", false);  //  RW-mode (second parameter false).
     currentStateVector.desiredTempLow = flashPreferences.getFloat("desiredTempLow", defaultLowTemp);
     currentStateVector.desiredTempHigh = flashPreferences.getFloat("desiredTempHigh", defaultHighTemp);
     currentStateVector.heatHigh = flashPreferences.getBool("heatHigh", false);
     flashPreferences.end();
 
-    mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
-    //mqttClient.setCallback(mqttReceiving);  // When received subscribed topics
-
     heatRegulator.setParameters(hysteresisUpTemp, hysteresisDownTemp, desiredTempMax, desiredTempMin);
 }
 
-void loop(){
-    /*if (! mqttClient.connected()) {
-        mqttReconnect();
-    }
-    mqttClient.loop();    // Listening for subscribed topics*/
+void loop() {
 
-    //Updating sensor data
+    // Updating sensor data
     float t = dht.readTemperature();
     float h = dht.readHumidity();
     if (isnan(h) || isnan(t)) {
@@ -147,7 +104,7 @@ void loop(){
     Serial.print("  HeatHigh: ");
     Serial.println(currentStateVector.heatHigh);
 
-    //Automatic control
+    // Automatic control
     if(currentStateVector.heatHigh)
         currentStateVector.relayOn = heatRegulator.getRegAction(currentStateVector.desiredTempHigh, currentStateVector.actualTemp);
     else
@@ -189,7 +146,7 @@ void loop(){
     snprintf (message, 20, "%.1f", currentStateVector.actualTemp);
     mqttClient.publish(tempTopic, message);*/
 
-    //Updating actuators accoring to current StateVector variables
+    // Updating actuators accoring to current StateVector variables
     digitalWrite(relayPin, currentStateVector.relayOn);
     ledcWrite(ledChannel, currentStateVector.displayBrightness);
 
